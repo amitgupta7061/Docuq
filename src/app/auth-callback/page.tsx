1a56d6b36ee6059c93e8d5a25a2181e3ebc06af0
Amit <amitgupta561710@gmai.com>
2025-06-04 23:08:11 +0530

    Intialize

---
+++ src/app/auth-callback/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { useRouter, useSearchParams } from "next/navigation"
+
+const Page = () => {
+
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const origin = searchParams.get('origin');
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Page
